7384273588b6c22378bf9cd4eb3af6cf0ce0ca67
Nuttanon-Kingkaewrungrote <6531503102@lamduan.mfu.ac.th>
2026-01-14 21:51:58 +0700

    fix margin in enter dice

--- src/components/DiceInput.jsx
+++ src/components/DiceInput.jsx
@@ -10,10 +10,10 @@ const DiceInput = ({
 }) => {
   return (
     <div 
-      className="bg-slate-50 rounded-xl p-4 mb-5 border border-neutral-300"
+      className="bg-slate-50 rounded-xl p-4 mb-6 border border-neutral-300"
       style={{width: '734px', height: '123px'}}
     >
-      <h2 className="px-4 text-md font-medium text-gray-800 mb-0">
+      <h2 className="px-2 text-md font-medium text-gray-800 mb-0">
         Enter dice numbers
       </h2>
       <div className="flex flex-wrap items-center justify-center gap-2">
@@ -26,25 +26,29 @@ const DiceInput = ({
               if (v.length <= 3) setManualInput(v);
             }}
             placeholder="Enter 3 digits"
-            className="w-[343px] h-[48px] text-center text-lg font-inter bg-slate-50 border border-neutral-300 rounded-lg focus:outline-none focus:border-blue-500"
+            className="text-center text-lg font-normal bg-slate-50 border border-neutral-300 rounded-lg focus:outline-none focus:border-blue-500"
+            style={{width: '343px', height: '48px'}}
           />
 
           <button
             onClick={onManualEntry}
-            className="px-6 py-3 bg-blue-600 text-white rounded-lg hover:bg-blue-500 font-semibold shadow transition-colors"
+            className="bg-blue-600 text-white rounded-lg hover:bg-blue-500 font-semibold shadow transition-colors"
+            style={{width: '100px', height: '48px'}}
           >
             Enter
           </button>
         </div>
         <button
           onClick={onClear}
-          className="px-6 py-3 bg-red-600 hover:bg-red-400 text-white font-semibold rounded-lg shadow transition-colors"
+          className="bg-red-600 hover:bg-red-400 text-white font-semibold rounded-lg shadow transition-colors"
+          style={{width: '100px', height: '48px'}}
         >
           Reset
         </button>
         <button
           onClick={onToggleHistory}
-          className="px-9 py-3 font-semibold rounded-lg shadow transition-colors bg-white border-2 border-blue-500 text-blue-500 hover:bg-blue-50"
+          className="font-semibold rounded-lg shadow transition-colors bg-white border-2 border-blue-500 text-blue-500 hover:bg-blue-50"
+          style={{width: '121px', height: '48px'}}
         >
           {showHistory ? '← Back' : 'History'}
         </button>
